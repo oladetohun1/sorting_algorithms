ca5e6bb3664aa2572838bbe4b085bbcc46c004ba
oladetohun1 <oladetohunoluwakorede@gmail.com>
2023-02-22 12:51:15 +0000

    corrected betty errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,14 +7,15 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i,index,tmp = 0;
+	size_t i, index, tmp = 0;
 
 	if (size < 2)
 		return;
 	for (i = 0; i < size; i++)
 		for (index = 0; index < size; index++)
 		{
-			if(array[index] > array[index + 1]){
+			if (array[index] > array[index + 1])
+			{
 				tmp = array[index];
 				array[index] = array[index + 1];
 				array[index + 1] = tmp;
